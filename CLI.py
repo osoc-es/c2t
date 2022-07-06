78d504be8fd60965eaf55157ad076e45635fb36d
David <DalaMartin28@gmail.com>
2022-07-06 16:36:45 +0200

    Updated CLI

--- CLI.py
+++ CLI.py
@@ -10,35 +10,38 @@ def cli():
 
 @cli.command()
 @click.argument('name')
-def image(name):
+@click.argument('output')
+def image(name, output):
     """ Inspect your image from its name or ID """
     os.system(('docker inspect '+ name))
     syft = os.popen(('syft '+name+' -o cyclonedx-json')).read()
     print(syft)
-    syft_parser.parse_data(syft, 'prueba')
+    syft_parser.parse_data(syft, output+'.json')
 
 
 @cli.command()
 @click.argument('name')
-def dockerhub(name):
+@click.argument('output')
+def dockerhub(name, output):
     """ Inspect an image from dockerhub """
     os.system(('docker pull '+ name))
     os.system(('docker inspect '+ name))
     syft = os.popen(('syft '+name+' -o cyclonedx-json')).read()
     print(syft)
-    syft_parser.parse_data(syft, 'prueba')
+    syft_parser.parse_data(syft, output+'.json')
 
 
 
 @cli.command()
 @click.argument('name')
-def dockerfile(name):
+@click.argument('output')
+def dockerfile(name, output):
     "Inspect your image from your local dockerfile. The name must be between 2 and 255 letters."
     os.system(("docker build . -t "+name))
     os.system(("docker inspect "+name))
     syft = os.popen(('syft '+name+' -o cyclonedx-json')).read()
     print(syft)
-    syft_parser.parse_data(syft, 'prueba')
+    syft_parser.parse_data(syft, output+'.json')
 
 
 if __name__ == '__main__':
