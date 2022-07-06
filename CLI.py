ee3d29b2361fc8a691ff40f93d3ebac81f4e8ead
David <DalaMartin28@gmail.com>
2022-07-06 15:50:25 +0200

    Update CLI

--- CLI.py
+++ CLI.py
@@ -13,7 +13,7 @@ def cli():
 def image(name):
     """ Inspect your image from its name or ID """
     os.system(('docker inspect '+ name))
-
+    os.system(('syft '+name+' -o cyclonedx-json'))
 
 @cli.command()
 @click.argument('name')
@@ -21,14 +21,15 @@ def dockerhub(name):
     """ Inspect an image from dockerhub """
     os.system(('docker pull '+ name))
     os.system(('docker inspect '+ name))
-
+    os.system(('syft '+name+' -o cyclonedx-json'))
 
 @cli.command()
 @click.argument('name')
 def dockerfile(name):
-    "Inspect your image from your local dockerfile"
+    "Inspect your image from your local dockerfile. The name must be between 2 and 255 letters."
     os.system(("docker build . -t "+name))
     os.system(("docker inspect "+name))
+    os.system(('syft '+name+' -o cyclonedx-json'))
 
 
 if __name__ == '__main__':
