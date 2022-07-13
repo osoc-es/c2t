a43df9a4ce8e748857d79dce79d3e78be1492e34
David <DalaMartin28@gmail.com>
2022-07-13 10:56:11 +0200

    syftchange

--- src/CLI.py
+++ src/CLI.py
@@ -3,13 +3,9 @@ import os
 import syft_parser as syft_parser 
 import morph_kgc
 
-def get_graph(name):
-    os.system(('docker inspect '+ name+' > ../data/inspect1.json'))
-    syft = os.popen(('syft '+name+' -o cyclonedx-json')).read()
-    syft_parser.parse_data(syft, '../data/result1.json')
-    g = morph_kgc.materialize('../config.ini')
-    g.serialize(destination="grafo.nt", format="ntriples")
-
+def get_graph():
+    #os.path.dirname(os.path.normpath(yourpath))
+    pass
 
 @click.group()
 def cli():
@@ -21,7 +17,11 @@ def cli():
 @click.argument('name')
 def image(name):
     """ Inspect your image from its name or ID """
-    get_graph(name)
+    os.system(('docker inspect '+ name+' > ../data/inspect1.json'))
+    syft = os.popen(('syft '+name+' -o cyclonedx-json')).read()
+    syft_parser.parse_data(syft, '../data/result1.json')
+    g = morph_kgc.materialize('../config.ini')
+    g.serialize(destination="grafo.nt", format="ntriples")
 
 
 @cli.command()
